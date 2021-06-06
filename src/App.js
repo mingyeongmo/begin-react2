df3383b9207f72664f1ea457da074fd2969fdc53
mingyeongmo <mingyeongmp@naver.com>
2021-06-06 18:09:30 +0900

    LifeCycle API 사용법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
-import Counter from './Counter';
+import MyComponent from './MyComponent';
+
 
 class App extends Component {
+  state = {
+    counter: 1,
+    error: false
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+    // API 를 통해서 서버로 오류 내용 날리기
+  }
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter +1
+    });
+  }
   render() {
-    return <Counter />;
+    if (this.state.error) {
+      return (
+        <div>에러가 났음</div>
+      )
+    }
+    return (
+      <div>
+        { this.state.counter < 10 && <MyComponent value={this.state.counter} /> }
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    )
   }
 }
 
