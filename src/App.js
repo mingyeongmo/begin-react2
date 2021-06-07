3282e7c1cc50591df0c8a41413fb08180f3ab656
mingyeongmo <mingyeongmp@naver.com>
2021-06-07 17:23:29 +0900

    input 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
-
+import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
-  state = {
-    counter: 1,
-    error: false
-  };
-
-  componentDidCatch(error, info) {
-    this.setState({
-      error: true,
-    });
-    // API 를 통해서 서버로 오류 내용 날리기
-  }
-
-  constructor(props) {
-    super(props);
-    console.log('constructor');
-  }
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
-  handleClick = () => {
-    this.setState({
-      counter: this.state.counter +1
-    });
-  }
+  
   render() {
-    if (this.state.error) {
-      return (
-        <div>에러가 났음</div>
-      )
-    }
     return (
       <div>
-        { this.state.counter < 10 && <MyComponent value={this.state.counter} /> }
-        <button onClick={this.handleClick}>Click Me</button>
+        <PhoneForm />
       </div>
     )
   }
